f665e093bf5cf06d0f942de5825d6ae49008a28e
BenRyan0 <benryanrinconada5@gmail.com>
2025-08-21 17:29:54 +0800

    Edit-Prep4Deployment -dev0

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/auth/login";
+import Login from "./pages/auth/Login";
 import Main from "./layout/main";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
@@ -9,28 +9,15 @@ import PayersList from "./pages/PayersList";
 import PayerProgress from "./pages/PayerProgress";
 import ChangePassword from "./pages/ChangePassword";
 import ProtectUser from "./utils/ProtectUser";
-// import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import {get_user_info} from "./store/reducers/authReducer"
-
-function App() {
-  // const dispatch = useDispatch()
-  // const {token} = useSelector(state=>state.auth)
-  //   useEffect(()=>{
-  //   if(token){
-  //     dispatch(get_user_info())
-  //   }
-  // },[token, dispatch])
 
 
-  
+function App() {  
   return (
        <Routes>
         <Route path="/login" element={<Login />} />
         {/* Protected routes */}
         <Route element={<ProtectUser />}>
           <Route element={<Main />}>
-            {/* <Route path="/" element={<Dashboard />} /> */}
             <Route path="/" element={<UserPaymentsList />} />
             <Route path="/payers" element={<PayersList />} />
             <Route path="/view-progress/:payerId" element={<PayerProgress />} />
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,32 +1,16 @@
 import React from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { useNavigate } from "react-router-dom"; // fixed from react-router
-// import { toast } from "react-toastify";
-
 import { LoginForm } from "../../components/login-form";
-// import { messageClear} from "../../store/reducers/authReducer"; // Make sure this path is correct
+
 
 const Login = () => {
-  // const dispatch = useDispatch();
-  // const navigate = useNavigate();
 
-  // const { userInfo, successMessage, errorMessage, redirect } = useSelector(
-  //   (state) => state.auth
-  // );
-
-
-
-  const handleSuccessLogin = (data) => {
-    console.log("Success from login.jsx:", data);
-    // You can optionally redirect here if needed
-  };
 
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-[#0A0A0A]">
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
 
       <div className="w-full max-w-sm relative z-10">
-        <LoginForm className="bg-[#171717]" onLogin={handleSuccessLogin} />
+        <LoginForm className="bg-[#171717]" />
       </div>
     </div>
   );
